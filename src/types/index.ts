eb7a7dd8fe1d6c50d16d76924507cbb0bade3bd3
Tima Baisalov <baisalov.t98@gmail.com>
2025-04-24 18:32:44 -0500

    feat(types): add Product, CartItem, and Filters interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export interface Product {
+    id: string;
+    title: string;
+    description: string;
+    brand: string;
+    image: string;
+    price: number;
+    rating: number;
+    category: 'phone' | 'laptop' | 'gadget';
+  }
+  
+  export interface CartItem {
+    product: Product;
+    quantity: number;
+  }
+  
+  export interface Filters {
+    priceRange: [number, number];
+    brands: string[];
+    rating: number;
+    category: 'phone' | 'laptop' | 'gadget' | 'all';
+  }
+  
