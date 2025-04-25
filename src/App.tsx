5736cbe3bde0cb3a9b416ce436793b18709db58d
Tima Baisalov <baisalov.t98@gmail.com>
2025-04-24 20:34:04 -0500

    feat(i18n): add dynamic translation loading with i18next and http backend

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import type { AppDispatch } from "@/store";
 import { BrowserRouter as Router, Routes, Route, Link  } from "react-router-dom";
 const CatalogPage = lazy(() => import("@/pages/CatalogPage"));
 const CartPage = lazy(() => import("@/pages/CartPage"));
+import { useTranslation } from "react-i18next";
 
 const App = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -14,13 +15,19 @@ const App = () => {
     dispatch(setProducts(mockProducts));
   }, [dispatch]);
 
+  const { t, i18n } = useTranslation();
+
   return (
     <Router>
     <header style={{ padding: 16, borderBottom: "1px solid #ccc", marginBottom: 24 }}>
       <nav>
-        <Link to="/" style={{ marginRight: 16 }}>Catalog</Link>
-        <Link to="/cart">Cart</Link>
+      <Link to="/" style={{ marginRight: 16 }}>{t("catalog")}</Link>
+      <Link to="/cart">{t("cart")}</Link>
       </nav>
+      <div style={{ marginTop: 8 }}>
+          <button onClick={() => i18n.changeLanguage("ru")}>ru</button>
+          <button onClick={() => i18n.changeLanguage("en")}>en</button>
+        </div>
     </header>
     <Suspense fallback={<div>Loading...</div>}>
         <Routes>
